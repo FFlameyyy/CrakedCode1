208d6e6b9460ca64047ec078dff425d6c3e3cb87
FFlameyyy <110302057+FFlameyyy@users.noreply.github.com>
2022-09-24 17:30:02 -0400

    Update script.lua

--- script.lua
+++ script.lua
@@ -1,11 +1,12 @@
 local a = game.Players.LocalPlayer.UserId
 wait(3)
-if a == 54568682 then
-	script.Parent.Parent.Parent.Locked.Visible = false
+script.Parent.Parent.Parent.Loader.Visible = true
+if a == 545686828 then
+	script.Parent.Parent.Parent.Loader.Visible = false
 else
-	script.Parent.Text = "You Are Not Authorized To Use This. Your Identification Did Not Match If This Was An Error, Please Try Again Later. The Main Scripted Has Been Removed, And This Screen Will Remove Shortly. Your Identification Code: ".. a
+	script.Parent.Text = "You Are Not Authorized To Use This. Your Identification Did Not Match If This Was An Error, Please Try Again Later. The Main Script Has Been Removed, And This Screen Will Remove Shortly. Anti-Theft Lock On The Main Scripted Has Been Enabled To Prevent Bypasses. Your Identification Code: ".. a
 	script.Parent.Parent.Parent.Joint.Visible = false
-	wait(5)
+	wait(20)
 	while true do
 wait()
 if script.Parent.Parent.Parent.Parent.CrackedScreenUi.Enabled == true then
