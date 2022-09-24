a124674c34ec3de4fab5e5f801a00109e7c407b1
FFlameyyy <110302057+FFlameyyy@users.noreply.github.com>
2022-09-24 16:55:05 -0400

    Update script.lua

--- script.lua
+++ script.lua
@@ -1,6 +1,6 @@
 local a = game.Players.LocalPlayer.UserId
-if a == 54568682 then
-script.Parent.Parent.Parent.Locked.Visible = false
+if a == 545686828 then
+script.Parent.Parent.Parent.Loader.Visible = false
 else
 script.Parent.Text = "You Are Not Authorized To Use This. You Identification Did Not Match If This Was An Error, Please Try Again Later. Your Identification Code: ".. a
 script.Parent.Parent.Parent.Joint.Visible = false
