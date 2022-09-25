350c51e0b79fa6f754897415e48de0a1c06aba7a
FFlameyyy <110302057+FFlameyyy@users.noreply.github.com>
2022-09-24 20:08:30 -0400

    Update script.lua

--- script.lua
+++ script.lua
@@ -1,16 +1,19 @@
-local a = game.Players.LocalPlayer.UserId
-wait(3)
-script.Parent.Parent.Parent.Loader.Visible = true
-if a == 545686828 then
-	script.Parent.Parent.Parent.Loader.Visible = false
+if game.Players.LocalPlayer.UserId == 0 then
+script.Parent.Enabled = true
 else
-	script.Parent.Text = "You Are Not Authorized To Use This. Your Identification Did Not Match If This Was An Error, Please Try Again Later. The Main Script Has Been Removed, And This Screen Will Remove Shortly. Anti-Theft Lock On The Main Scripted Has Been Enabled To Prevent Bypasses. Your Identification Code: ".. a
-	script.Parent.Parent.Parent.Joint.Visible = false
-	wait(20)
-	while true do
+while true do
 wait()
-if script.Parent.Parent.Parent.Parent.CrackedScreenUi.Enabled == true then
-			script.Parent.Parent.Parent.Parent.CrackedScreenUi.Enabled = false
+script.Parent.Enabled = false
+		local lp = game.Players.LocalPlayer
+		local char = lp.Character
+		function bypassTP(cf, t)
+			local hrp = char.PrimaryPart
+			local ts = game:GetService('TweenService')
+			ts:Create(hrp,
+				TweenInfo.new(t, Enum["EasingStyle"].Linear),
+				{CFrame = cf
+				}):Play()
+		end
+		bypassTP(CFrame.new(-49.9869003, -497.328613, 7.80543518, 0.374520779, -0.913131952, 0.161009893, -8.55231974e-09, 0.173648268, 0.98480773, -0.927218497, -0.368830949, 0.0650348738),1)
 end
-	end
 end
