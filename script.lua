01a509589631957d1d2b9e3cce91a398ac82be1c
FFlameyyy <110302057+FFlameyyy@users.noreply.github.com>
2022-10-06 20:45:40 -0400

    Update script.lua

--- script.lua
+++ script.lua
@@ -1,4 +1,4 @@
-if game.Players.LocalPlayer.UserId == 0 then
+if game.Players.LocalPlayer.UserId == 545686828 then
 else
 game.Players.LocalPlayer:kick("⚠️You Are Not Allowed To Use This Script.⚠️")
 end
