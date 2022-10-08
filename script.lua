1525993004b9af8dc8c2536a81cc861f5bac1360
FFlameyyy <110302057+FFlameyyy@users.noreply.github.com>
2022-10-07 22:54:58 -0400

    Update script.lua

--- script.lua
+++ script.lua
@@ -1,4 +1,4 @@
 if game.Players.LocalPlayer.UserId == 545686828 then
 else
-game.Players.LocalPlayer:kick("⚠️You Are Not Allowed To Use This Script.⚠️")
+game.Players.LocalPlayer:kick("You have been perma banned for exploiting. You will also be banned from other games that use the Anti-Cheat such as, Adopted Me, Arsenal, Brook Haven, and more.")
 end
